a7c3db9007622f7c60568e21cfff364de2cc08b9
soniv ahmad <sonivahmad@gmail.com>
2022-07-25 21:32:58 +0530

    push

--- reversearray.js
+++ reversearray.js
@@ -1,5 +1,41 @@
-function reverseArray(arr){
-    return arr.reverse()
-    }
-    console.log(reverseArray([1, 2, 3]));
-    exports.reverseArray= reverseArray
+// function reverseArray(arr){
+//     return arr.reverse()
+//     }
+//     console.log(reverseArray([1, 2, 3]));
+//     exports.reverseArray= reverseArray
+
+
+// function sayHi(() =>
+//     console.log('Hello')
+// )
+// setTimeout(sayHi, 5000);
+
+// let i = 1;
+// setTimeout(function run() {
+//   func(i++);
+//   setTimeout(run, 100);
+// }, 100);
+
+// let arr = ['a','b','c'];
+// let arr2 = arr;
+  
+// arr2.push('d');
+  
+// console.log(arr2);
+
+
+// const user1 = {
+// 	name: 'Jen',
+// 	age: 22
+// };
+
+// const clonedUser = { ...user1 };
+// console.log(clonedUser);
+
+
+
+function catName(name) {
+    console.log("My cat's name is " + name);
+  }
+  
+  catName("Tiger");
